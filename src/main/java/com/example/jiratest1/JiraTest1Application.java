24dffb1fa481917ce510dac4cd0902149ab14f83
seosang-won <zmdk1205@naver.com>
2023-07-27 11:18:42 +0900

    initial commits

---
+++ src/main/java/com/example/jiratest1/JiraTest1Application.java
@@ -0,0 +1,14 @@
+package com.example.jiratest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiraTest1Application.class, args);
+
+    }
+
+}
