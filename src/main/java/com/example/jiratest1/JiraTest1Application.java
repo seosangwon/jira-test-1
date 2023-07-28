42d8e1e8673f2505c8525b3e419bd0e639288eb3
seosang-won <zmdk1205@naver.com>
2023-07-28 15:55:06 +0900

    LKLN-24 Feat: login다시 내용: 나는 지금 로그인 개발 중

--- src/main/java/com/example/jiratest1/JiraTest1Application.java
+++ src/main/java/com/example/jiratest1/JiraTest1Application.java
@@ -9,6 +9,7 @@ public class JiraTest1Application {
     public static void main(String[] args) {
         SpringApplication.run(JiraTest1Application.class, args);
         System.out.println("지라 1번째 테스트입니다!");
+        System.out.println("나는 지금 로그인 개발 중");
 
     }
 
